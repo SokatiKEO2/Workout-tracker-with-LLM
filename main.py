eaf5e1979c5f3c4c499ffc787c13351f9c9f466e
Sokati <2023475keo@aupp.edu.kh>
2024-03-03 15:38:02 +0700

    done

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
 from dotenv import dotenv_values
 import requests
+import datetime as dt
 
 
-SECRET_API = dotenv_values(".env.dev")
+SECRET_API = dotenv_values(".env")
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
+exercise_text = input("Tell me what exercise you did: ")
+weight = 78
+height = 172
+age = 20
+
+headers = {
+    "x-app-id" : SECRET_API["APP_ID"],
+    "x-app-key" : SECRET_API["API_KEY"]
+}
+
+parameters = {
+    "query" : exercise_text,
+    "weight_kg" : weight,
+    "height_cm" : height,
+    "age" : age,
+}
+
+response = requests.post(url=exercise_endpoint, headers=headers, json=parameters)
+result = response.json()
+print(result)
+
+date = dt.datetime.now().strftime("%d/%m/%Y")
+time = dt.datetime.now().strftime("%X")
+
+SHEET_ENDPOINT = SECRET_API["SHEET_ENDPOINT"]
+GOOGLE_SHEET_NAME = "workout"
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        GOOGLE_SHEET_NAME: {
+            "date": date,
+            "time": time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+sheet_response = requests.post(SHEET_ENDPOINT, json=sheet_inputs)
+print(f"Sheety Response: \n {sheet_response.text}")
+    
